186cb0b06c8294b77fb25b97a47010e515b0a160
Joshua Hawxwell <m@hawx.me>
2026-07-23 11:54:55 +0100

    Add the OneLogin and Shared Spaces prototype

--- app/routes.js
+++ app/routes.js
@@ -6,4 +6,19 @@
 const govukPrototypeKit = require('govuk-prototype-kit')
 const router = govukPrototypeKit.requests.setupRouter()
 
-// Add your routes here
+router.post('/redirect', (req, res) => {
+  const referer = req.header('referer');
+
+  const redirect = req.session.data['redirect'];
+  req.session.data['redirect'] = null;
+
+  if (redirect) {
+    const parts = referer.split('/');
+    parts.pop();
+    parts.push(redirect);
+
+    res.redirect(parts.join('/'));
+  } else {
+    res.redirect(referer);
+  }
+});
